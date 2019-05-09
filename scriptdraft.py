281e82114e900d49a72bfeb0ed3a7e9b2160bb5f
Anna Weir <49284980+acw414@users.noreply.github.com>
2019-05-09 14:54:25 -0700

    Update scriptdraft.py

--- scriptdraft.py
+++ scriptdraft.py
@@ -3,7 +3,7 @@
 ```
 This script takes an eDNA output file (with columns of different locations and taxonomy information in the last one), and will extract the species name (if applicable)
 
-Usage = python2 scriptdraft.py inputfile geographicregion
+Usage = python2 scriptdraft.py inputfile 
 
 ```
 
@@ -13,15 +13,18 @@ import sys
 import os
 
 inputfile = sys.argv[1]
-create a file called unusable.txt 
+
+open(unusable.txt,"w+")
 
 with open(inputfile, "r") as my_file:
-	for each column after the 1st one, not including the last one:
+	for each column after the first (not including the last one):
+		counter_columnnumber = 0
 		make a temporary file called columnheader.txt
 		go down each column and if it comes across a value != 0:
 			if there is a white space after the last ;:
-				<colummnname> : <entry in the last column of that line (the taxonomic data)?> >> unusable.txt
+				<colummnname> : <taxonomic data> >> unusable.txt
 			if there is no white space after the last ;:
+				counter_columnnumber = counter_columnnumber + 1
 				split the line at the last ;
 				grab the last item in the line (the species name)
 				save the speies in the temporary file for that line
