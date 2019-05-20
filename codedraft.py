b6c356baeef38e125bd73b70cd9be4438153cd9f
Anna Weir <49284980+acw414@users.noreply.github.com>
2019-05-20 09:40:00 -0700

    Update codedraft.py

--- codedraft.py
+++ codedraft.py
@@ -4,20 +4,26 @@
 
 #Usage = python2 scriptdraft.py inputfile  
 
+#this just imports some functions
 import re
 import string 
 import sys
 import os
 
+#this assgns the variable inputfile to the inputfile
 inputfile = sys.argv[1]
 
 openfile = open(inputfile, "r")
 for line in openfile:	
 	Line = line.strip()
 	#print Line	
-	Col = line.split("\t")
-	#print Col[0]
-	if Col[1] != "0": #if the item in the 2nd column does not equal 0
+	Col = line.split("\t") #ok up to this point this has made a list of tuples called Col. everything above here is ok i think.
+	#U can do "print Col" to check out how Col is organized.
+	#Col a list containing other lists - each column is split into a list.
+	#and you can call them too. like 'print Col[0]' will print everything in the 1st column. 
+	#OH YEAH EVERYTIME U USE 'COL' ITS GOT TO BE (AT LEAST) AT THIS LEVEL OF INDENT bc the variable only exists within this for loop. 
+	if Col[1] != "0": #if the item in the 2nd column does not equal 0. Daniel said we dont have to do lots of for loops and can go straight into an if statement. 
+		#but we want it to go through all the lists in Col, not just Col[1], so ya we need to work that out.
 		print "yaya" #this was just a placeholder to see if it worked. sorry
 		#it should split the last item in the row at the last ; and then 
 		#if there is a blank space after then that whole last chunk of taxonomy data gets added to a list called 'unusable, or sth. 
@@ -25,4 +31,5 @@ for line in openfile:
 	if Col[1] == "0":
 		print "no" #osryr thiis was a placeholder too
 		#if no then do nothing? maybe we dont even include a no statement?
-#this should repeat for all columns except Col[0] (the 1st one) and Col[-1] (the last one w the species data
+	
+#this should repeat for all columns except Col[0] (the 1st one) and Col[-1] (the last one w the species data)
