6eb42dd343c06d603922220b7db804d392cc325f
Anna Weir <49284980+acw414@users.noreply.github.com>
2019-05-22 16:17:39 -0700

    Update codedraft.py

--- codedraft.py
+++ codedraft.py
@@ -16,22 +16,9 @@ outputCG=open("sample1.txt", "w")
 for line in openfile:   
         Line = line.strip()     
         Col = line.split("\t")
-        if Col[1] != "0" and len(Col) >3: #if the item in the 2nd column does not equal 0
-                Elementlist = line.split(';')[-1]       
-                #sample1 = u/home/class/c177/c177-28/Project/sample1.txt                
-                #f = open(sample1,"w+")
-                sample1.append(Elementlist)
-        #       Elementlist >> sample1.txt  #prints the last thing in the line (the species)
-                outputCG.write(Elementlist)
-
-                #       print "noo"
-#               else:           
-#                       print 'cannae'
-#print "yaya" #this was just a placeholder to see if it worked. sorry 
-                #if there is a blank space after then that whole last chunk of taxonomy data gets added to a list called 'unusable, or sth. 
-#       if Col[1] == "0":
-#               print "no" #osryr thiis was a placeholder too
-                #if no then do nothing? maybe we dont even include a no statement?
-#this should repeat for all columns except Col[0] (the 1st one) and Col[-1] (the last one w the species data
+        if Col[1] != "0" and len(Col) >3: #if the item in the 2nd column does not equal 0 and the number of columns is > 3
+                Elementlist = line.split(';')[-1] #split the line at the last ;      
+                sample1.append(Elementlist) #append this to the list sample1 (in line 13)
+                outputCG.write(Elementlist) #write elementlist to the temp file outputCG. idk how this is different ot the line above or why u write Elementlist and not sample1?? idk confusing
 
 outputCG.close()
