89250e6dbf33454de19e04398669f5615cee2b6d
sohil2710 <49290215+sohil2710@users.noreply.github.com>
2019-05-22 18:37:06 -0700

    Update codedraft.py

--- codedraft.py
+++ codedraft.py
@@ -16,7 +16,7 @@ outputCG = open("sample1.txt", "w")
 for line in openfile:   
         Line = line.strip()     
         Col = line.split("\t")
-        if Col[1] != "0" or Col[3] != '\n' and len(Col) >3: #if the item in the 2nd column does not equal 0 and the number of columns is > 3
+        if Col[1] != "0" and len(Col) >3: #if the item in the 2nd column does not equal 0 and the number of columns is > 3
                 Elementlist = line.split(';')[-1] #split the line at the last ;      
                 sample1.append(Elementlist) #append this to the list sample1 (in line 13)
                 outputCG.write(Elementlist) #write elementlist to the temp file outputCG. idk how this is different ot the line above or why u write Elementlist and not sample1?? idk confusing
