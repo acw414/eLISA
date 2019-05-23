a910188ad1d45d5bcd876a4967d94e3ae658922e
Anna Weir <49284980+acw414@users.noreply.github.com>
2019-05-22 19:18:28 -0700

    Update codedraft.py

--- codedraft.py
+++ codedraft.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python2
 
-#This script takes an eDNA output file (with columns of different locations and taxonomy information in the last one), and will extract the species name (if applicable)
+#This script takes the Fish_taxonomy_file.txt, and will output a text file per sample location containing all the species sampled there (with no repeats)
 
-#Usage = python2 codedraft.py inputfile 
-#we are using Fish_taxonomy_file.txt as inputfile
+#Usage = python2 codedraft.py Fish_taxonomy_file.txt
 
 import re
 import string
@@ -14,23 +13,39 @@ inputfile = sys.argv[1]
 openfile = open(inputfile, "r")
 sample1 = []
 sample2 = []
-output1 = open("sample1.txt", "w")
-output2 = open("sample2.txt", "w")
+output1 = open("s1temp.txt", "w")
+output2 = open("s2temp.txt", "w")
 for line in openfile:
         Line = line.strip()
         Col = line.split("\t")
         if Col[1] != "0" and len(Col) >3: #if the item in the 2nd column does not equal 0 and the number of columns is > 3
                 Elementlist = line.split(';')[-1] #split the line at the last ;
-                sample1.append(Elementlist) #append this to the list sample1 (in line 13)
-                output1.write(Elementlist) #write elementlist to the temp file outputCG. idk how this is different ot the line above or why u write Elementlist and not sample1?? idk confusing
+                sample1.append(Elementlist)
+                output1.write(Elementlist)
         if Col[2] != "0" and len(Col) >3:
                 Element_list = line.split(';')[-1]
                 sample2.append(Element_list)
                 output2.write(Element_list)
 
-#need to work out how to do this for all columns in a doc, each creating a new file called sample1 sample2 etc.
-#and yeah mauybe we dont need to do the count for Thurs, i can do that after
 output1.close()
 output2.close()
-#need to find a way to remove instances of duplication in output file
-#need to assign the specific sample name to the output file row1, not the entire 1st line from input file
+
+unique1 = set() # holds unique species in sample 1
+outfile1 = open("sample1.txt", "w")
+for line in open("s1temp.txt", "r"):
+        if line not in unique1: #if the species is not a duplicate
+                outfile1.write(line)
+                unique1.add(line)
+                
+outfile1.close()
+
+os.remove("s1temp.txt")
+unique2 = set() # holds unique species in sample 2
+outfile2 = open("sample2.txt", "w")
+for line in open("s2temp.txt", "r"):
+        if line not in unique2: #if the species is not a duplicate
+                outfile2.write(line)
+                unique2.add(line)
+os.remove("s2temp.txt")
+
+outfile2.close()
