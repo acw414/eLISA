84f79f9e04d4fa03280094771e3f1e734727ef17
Anna Weir <49284980+acw414@users.noreply.github.com>
2019-05-30 14:37:45 -0700

    Update FinalBashScript.sh

--- FinalBashScript.sh
+++ FinalBashScript.sh
@@ -5,17 +5,26 @@
 #It will just take the file to be analyzed as the input
 #Usage = sh bashscript inputfile.txt
 
+inputerror=".........
+WARNING: INPUT ERROR.
+/namebofourprogram/ requires one input - the name of your input file.
+Example usage:
+	sh codedraft.py inputfile.txt
+Please check input and try again
+........."
+
 . /u/local/Modules/default/init/modules.sh
 
-module load python/3
-
-echo "........."
-echo "sorting input file"
-echo "........."
-
-python codedraft1.py $1
-
-sed -i '/^$/d' finalsamplecolumn*.txt
-
-echo "searching spceies database"
-echo "........."
+if [ "$#" != "1" ]; then
+        echo "$inputerror"
+else
+    	module load python/3
+        echo "........."
+        echo "sorting input file"
+        python codedraft1.py $1
+        echo "........."
+        sed -i '/^$/d' finalsamplecolumn*.txt
+        echo "searching spceies database"
+        echo "........."
+        #load R and run the R code
+fi
