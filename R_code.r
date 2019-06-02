1c362985102e4da19557aee81af28b49a2539085
sohil2710 <49290215+sohil2710@users.noreply.github.com>
2019-06-01 19:31:09 -0700

    Update R_code.r

--- R_code.r
+++ R_code.r
@@ -4,22 +4,18 @@
 library("tidyverse")
 library("originr")
 
-
-#invasive = list()
-
-setwd('~/Project_local')
+sink('output.txt')
+setwd('~/Project_local') #change this to current directory
 files <- list.files(path=".", pattern="finalsamplecolumn.*.txt", full.names=TRUE, recursive=FALSE)
 lapply(files, function(x) {
   t <- read.csv(x, header=TRUE) # load file
   t <- t[[1]]
+  print('New file')
   out <- for(species in t){
-      presence <- gisd(species)
-      print(presence)
-        
-      
-    
+    presence <- gisd(species)
+    print(presence)
   }
-  #write.csv(out, "~/Project_local/output.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
 })
+sink()
 #invasive_per_file = (number of invasive species/number of total species)*100
 #need to produce a file that has a table with name of sample file, %invasive, print list invasive, and native_range
