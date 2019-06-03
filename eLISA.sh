196586bd2effd8a8353d0f9b899ea27db63aa3a2
Anna Weir <49284980+acw414@users.noreply.github.com>
2019-06-03 09:05:20 -0700

    Update eLISA.sh

--- eLISA.sh
+++ eLISA.sh
@@ -9,7 +9,7 @@ inputerror=".........
 WARNING: INPUT ERROR.
 /namebofourprogram/ requires one input - the name of your input file.
 Example usage:
-	sh codedraft.py inputfile.txt
+	sh eLISA.py inputfile.txt
 Please check input and try again
 ........."
 
@@ -21,14 +21,14 @@ else
     	module load python/3.1
         echo "........."
         echo "sorting input file"
-        python finalpythoncode.py $1
+        python eLISA_python_code.py $1
         echo "........."
         sed -i '/^$/d' finalsamplecolumn*.txt
         echo "searching species database"
         module load R/3.4.0
-        R CMD BATCH R_code.r
+        R CMD BATCH eLISA_R_code.r
         echo "........."
         echo "finalizing output table"
-#	rm ./finalsamplecolumn*.txt
+	rm ./finalsamplecolumn*.txt
         echo "........."
 fi
